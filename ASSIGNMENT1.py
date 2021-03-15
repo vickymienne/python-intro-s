ba30994209b76b6e6ea37b01a6294f94b2a6df4f
vickymienne <vickyjoeece@gmail.com>
2021-03-15 18:03:18 +0100

    ghfhg

--- ASSIGNMENT1.py
+++ ASSIGNMENT1.py
@@ -1,10 +1,10 @@
 import turtle
 init = 20
 turtle.color('red')
-for i in range(10):
+for i in range(1,11):
     turtle.penup()
     turtle.goto(0,-init*i)
     turtle.pendown()
 
-    turtle.circle(init*i+1)
+    turtle.circle(init*i)
 
